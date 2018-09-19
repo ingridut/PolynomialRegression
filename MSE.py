786546aec09d27bc1c9020f53cfabae2d8ffabda
Ingrid Utseth <ingrid.utseth@fys.uio.no>
2018-09-19 13:35:50 +0200

    test MSE in SimpleNoiseTest

--- MSE.py
+++ MSE.py
@@ -1,9 +1,24 @@
 import numpy as np
 
-def MeanSquaredError(y, y_hat):
+def MeanSquaredError(x, y, z, beta):
+    """
+    Calculates the Mean Squared Error
+    :param y: numpy vector with y data, size (n, 1)
+    :param x: numpy vector with x data, size (n, 1)
+    :param beta: model
+    :return: Mean squared error
+    """
+    # Calculate z_hat, the predicted z-values
+    M = np.c_[np.ones((100 ** 2, 1)), x, y, x ** 2, x * y, y ** 2,\
+                 x ** 3, x ** 2 * y, x * y ** 2, y ** 3,\
+                 x ** 4, x ** 3 * y, x ** 2 * y ** 2, x * y ** 3, y ** 4,\
+                 x ** 5, x ** 4 * y, x ** 3 * y ** 2, x ** 2 * y ** 3, x * y ** 4, y ** 5]
+    z_hat = M.dot(beta)
+
+    # Calculate MSE
     MSE = 0
-    for i in range(0, len(y)-1):
-        MSE += np.power(y(i) - y_hat(i), 2)
-    MSE = MSE/len(y)
+    for i in range(0, len(z)):
+        MSE += np.power(z[i, 0] - z_hat[i, 0], 2)
+    MSE = MSE/len(z)
 
     return MSE
--- SimpleNoiseTest.py
+++ SimpleNoiseTest.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from MSE import MeanSquaredError
 
 def runTest(degree=5):
 
@@ -41,6 +42,11 @@ def runTest(degree=5):
                  x ** 4, x ** 3 * y, x ** 2 * y ** 2, x * y ** 3, y ** 4,\
                  x ** 5, x ** 4 * y, x ** 3 * y ** 2, x ** 2 * y ** 3, x * y ** 4, y ** 5]
     predict = Mnew.dot(beta)
+
+    # calculate MSE:
+    z = FrankeFunction(x, y)
+    MSE = MeanSquaredError(x, y, z, beta)
+    print('MSE: ', MSE)
 
     # Plot the prediction
     fig = plt.figure()
