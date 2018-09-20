9a84b383945e95c272f4371a4503d450bfcbbfad
henriklg <henrik.logj@gmail.com>
2018-09-20 15:11:52 +0200

    moved franke func to new file

--- franke.py
+++ franke.py
@@ -11,9 +11,11 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
+from frankeFunction import FrankeFunction
 from random import random, seed
 
-## TODO: fikse registrering av trykk under skriving og autocorrect
+## TODO:
+
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
@@ -22,14 +24,6 @@ ax = fig.gca(projection='3d')
 x = np.arange(0, 1, 0.05)
 y = np.arange(0, 1, 0.05)
 x, y = np.meshgrid(x,y)
-
-
-def FrankeFunction(x,y):
-    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
-    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
-    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
-    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
-    return (term1 + term2 + term3 + term4)
 
 z = FrankeFunction(x,y)
 
