90af0ab1a15dc8848c42e36379616cd49e897a35
henriklg <henrik.logj@gmail.com>
2018-09-21 09:26:31 +0200

    made script a function

--- LassoRegression.py
+++ LassoRegression.py
@@ -3,82 +3,46 @@ import numpy as np
 from sklearn import linear_model
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import PolynomialFeatures
 from frankeFunction import FrankeFunction
 from scipy import misc
+
+def LassoRegression(x, y, z, degree=5, l=0.1):
+    # Arranging data into 2x50 matrix
+    a = np.array(x) #inputs
+    b = np.array(y) #outputs
+
+    #Split into training and test
+    X_train = a[:15, np.newaxis]        # picks 15 first elements of x
+    X_test = a[15:, np.newaxis]         # picks 5 (remaining) last elements of x
+    Y_train = b[:15]                    # same, but for y axis
+    Y_test = b[15:]
+    ##########
+
+
+    print ("X_train: ", X_train.shape)
+    print ("y_train: ", Y_train.shape)
+    print ("X_test: ", X_test.shape)
+    print ("y_test: ", Y_test.shape)
+
+    lasso = PolynomialFeatures(degree)
+    lasso = linear_model.Lasso(alpha=0.1)
+    lasso.fit(X_train, Y_train)             # lasso.fit([x,y],z)
+    predl = lasso.predict(X_test)
+    print ("Lasso Coefficient: ", lasso.coef_)
+    print ("Lasso Intercept: ", lasso.intercept_)
+    plt.scatter(X_test, Y_test,color='green', label="Training Data")
+    plt.plot(X_test, predl, color='blue', label="Lasso degree: %d" % degree)
+    plt.legend()
+    plt.show()
+    return 5
 
 ##########
 #terrain1 = misc.imread('data.tif',flatten=0)
 
-x1 = np.arange(0, 1, 0.05)
-y1 = np.arange(0, 1, 0.05)
-x1, y1 = np.meshgrid(x1,y1)
+x = np.arange(0, 1, 0.05)
+y = np.arange(0, 1, 0.05)
+#x, y = np.meshgrid(x, y)
 
-z1 = FrankeFunction(x1,y1)
-print(z1)
-##########
-
-
-# Creating data with random noise
-x=np.arange(50)
-
-delta=np.random.uniform(-2.5,2.5, size=(50))
-np.random.shuffle(delta)
-y = 0.5*x+5+delta
-
-# Arranging data into 2x50 matrix
-a = np.array(x) #inputs
-b = np.array(y) #outputs
-
-#Split into training and test
-X_train = a[:37, np.newaxis]
-X_test = a[37:, np.newaxis]
-Y_train = b[:37]
-Y_test = b[37:]
-
-print ("X_train: ", X_train.shape)
-print ("y_train: ", Y_train.shape)
-print ("X_test: ", X_test.shape)
-print ("y_test: ", Y_test.shape)
-
-print ("------------------------------------")
-
-print ("Ordinary Least Squares")
-# Add Ordinary Least Squares fit
-reg = LinearRegression()
-reg.fit(X_train, Y_train)
-pred = reg.predict(X_test)
-print ("Prediction Shape: ", pred.shape)
-print ("Coefficients: ", reg.coef_)
-# The mean squared error
-print ("Mean squared error: %.2f" % mean_squared_error(Y_test, pred))
-# Explained variance score: 1 is perfect prediction
-print ("Variance score: %.2f" % r2_score(Y_test, pred))
-
-# Plot
-plt.scatter(X_test,Y_test,color='green', label="Training Data")
-plt.plot(X_test, pred, color='black', label="Fit Line")
-plt.legend()
-plt.show()
-
-print ("------------------------------------")
-
-print ("Ridge Regression:")
-
-ridge = linear_model.RidgeCV(alphas=[0.1,1.0,10.0])
-ridge.fit(X_train, Y_train)
-print ("Ridge Coefficient: ",ridge.coef_)
-print ("Ridge Intercept: ", ridge.intercept_)
-# Look into graphing with Ridge fit
-
-print ("------------------------------------")
-
-print ("Lasso:")
-lasso = linear_model.Lasso(alpha=0.1)
-lasso.fit(X_train, Y_train)             # lasso.fit([x,y],z)
-predl = lasso.predict(X_test)
-print ("Lasso Coefficient: ", lasso.coef_)
-print ("Lasso Intercept: ", lasso.intercept_)
-plt.scatter(X_test, Y_test,color='green', label="Training Data")
-plt.plot(X_test, predl, color='blue', label="Lasso")
-plt.legend()
-plt.show()
+z = FrankeFunction(x, y)
+beta = LassoRegression(x,y,z)
