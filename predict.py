564f99c7403691ef15df5f2e85321e4bc7223064
betinahw <42771576+betinahw@users.noreply.github.com>
2018-09-21 10:44:37 +0200

    prediksjonsfunksjon (bør brukes av alle!)

---
+++ predict.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 21 10:40:11 2018
+
+@author: Betina
+"""
+import numpy as np
+def predict(x, y, z, beta):
+    """
+    Calculates the Mean Squared Error
+    :param y: numpy vector with y data, size (n, 1)
+    :param x: numpy vector with x data, size (n, 1)
+    :param beta: model
+    :return: Mean squared error
+    """
+    # Calculate z_hat, the predicted z-values
+    M = np.c_[np.ones((len(x), 1)), x, y, x ** 2, x * y, y ** 2,\
+                 x ** 3, x ** 2 * y, x * y ** 2, y ** 3,\
+                 x ** 4, x ** 3 * y, x ** 2 * y ** 2, x * y ** 3, y ** 4,\
+                 x ** 5, x ** 4 * y, x ** 3 * y ** 2, x ** 2 * y ** 3, x * y ** 4, y ** 5]
+    return M.dot(beta)
