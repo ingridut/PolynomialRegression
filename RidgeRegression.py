d7d31773151f8069684ab151b6dd003d8703e8d6
Ingrid Utseth <ingrid.utseth@fys.uio.no>
2018-09-22 14:41:08 +0200

    commute any degree Ridge

--- RidgeRegression.py
+++ RidgeRegression.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
 
 def RidgeRegression(x, y, z, degree=5, l=0.1):
     """
@@ -10,23 +11,15 @@ def RidgeRegression(x, y, z, degree=5, l=0.1):
     :return: numpy array, size (rows, 1)
 
     """
-    """
-    A = np.arange(1, degree+2)
-    rows = np.sum(A)
-    M = np.empty((len(x), rows))
-    row = 0
-    for i in range(degree+1):
-        for j in range(degree+1):
-            if i+j <= 5:
-                M[:, row] = np.power(x[:, 0], i)*(np.power(y[:, 0], j))
-                row += 1
-    """
-
+    M_ = np.c_[x, y]
+    poly = PolynomialFeatures(degree)
+    M = poly.fit_transform(M_)
     # Calculate matrix with powers of x and y
-    M = np.c_[np.ones((len(x), 1)), x, y, x ** 2, x * y, y ** 2,\
-                 x ** 3, x ** 2 * y, x * y ** 2, y ** 3,\
-                 x ** 4, x ** 3 * y, x ** 2 * y ** 2, x * y ** 3, y ** 4,\
-                 x ** 5, x ** 4 * y, x ** 3 * y ** 2, x ** 2 * y ** 3, x * y ** 4, y ** 5]
+    # M = np.c_[np.ones((len(x), 1)), x, y, x ** 2, x * y, y ** 2,\
+    #              x ** 3, x ** 2 * y, x * y ** 2, y ** 3,\
+    #              x ** 4, x ** 3 * y, x ** 2 * y ** 2, x * y ** 3, y ** 4,\
+    #              x ** 5, x ** 4 * y, x ** 3 * y ** 2, x ** 2 * y ** 3, x * y ** 4, y ** 5]
+
 
     # Calculate beta
     A = np.arange(1, degree + 2)
