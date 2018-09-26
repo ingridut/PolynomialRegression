2faef0dbf345d89f47e61b411f1471f49ddcf096
Polina <polinadh@gmail.com>
2018-09-26 18:02:05 +0200

    Add new version of function file

--- functionDefinitionsOLS.py
+++ functionDefinitionsOLS.py
@@ -1,9 +1,4 @@
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-from random import randrange, uniform
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
 import numpy as np
 from sklearn.preprocessing import PolynomialFeatures
 
